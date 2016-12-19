ac12adc979db4cefd96a9ed21ebaa6adaca6b565
xujin1982 <junxiaozhu@gmail.com>
2016-12-19 16:40:51 -0600

    Correction based on solution

--- Module6/1_Retrieve Product Price Information.sql
+++ Module6/1_Retrieve Product Price Information.sql
@@ -4,10 +4,8 @@
 SELECT ProductID, Name, ListPrice
 FROM SalesLT.Product
 WHERE ListPrice >
-	(SELECT AVG(ListPrice)
-	FROM SalesLT.Product AS p
-	JOIN SalesLT.SalesOrderDetail AS sod
-	ON p.ProductID = sod.ProductID)
+	(SELECT AVG(UnitPrice)
+	FROM SalesLT.SalesOrderDetail)
 ORDER BY ProductID;
 
 --2. Retrieve Products with a list price of $100 or more that have been sold for less than
@@ -16,36 +14,33 @@ ORDER BY ProductID;
 --the product has been sold for less than $100.
 SELECT ProductID, Name, ListPrice
 FROM SalesLT.Product
-WHERE ListPrice > 100
-UNION ALL
-SELECT p.ProductID, p.Name, p.ListPrice
-FROM SalesLT.Product AS p
-JOIN SalesLT.SalesOrderDetail AS sod
-ON p.ProductID = sod.ProductID
-WHERE p.ListPrice < 100;
+WHERE ProductID IN
+	(SELECT ProductID FROM SalesLT.SalesOrderDetail
+	WHERE UnitPrice < 100.00)
+AND ListPrice >= 100.00
+ORDER BY ProductID;
 
 --3. Retrieve the cost, list price, and average selling price for each product
 --Retrieve the product ID, name, cost, and list price for each product along with the average unit price for
 --which that product has been sold.
-SELECT DISTINCT p.ProductID, p.Name, p.StandardCost AS COST, p.ListPrice, 
+SELECT ProductID, Name, StandardCost, ListPrice, 
 	(SELECT AVG(UnitPrice)
 	FROM SalesLT.SalesOrderDetail AS sod
-	WHERE p.ProductID = sod.ProductID) AS AvgUnitPrice
+	WHERE p.ProductID = sod.ProductID) AS AvgSellingPrice
 FROM SalesLT.Product AS p
-JOIN SalesLT.SalesOrderDetail AS sod
-ON p.ProductID = sod.ProductID
+ORDER BY p.ProductID;
+
 
 --4. Retrieve products that have an average selling price that is lower than the cost
 --Filter your previous query to include only products where the cost price is higher than the average
 --selling price.
-SELECT DISTINCT p.ProductID, p.Name, p.StandardCost AS COST, p.ListPrice, 
+SELECT p.ProductID, p.Name, p.StandardCost AS COST, p.ListPrice, 
 	(SELECT AVG(UnitPrice)
 	FROM SalesLT.SalesOrderDetail AS sod
-	WHERE p.ProductID = sod.ProductID) AS AvgUnitPrice
+	WHERE p.ProductID = sod.ProductID) AS AvgSellPrice
 FROM SalesLT.Product AS p
-JOIN SalesLT.SalesOrderDetail AS sod
-ON p.ProductID = sod.ProductID
 WHERE p.StandardCost >
 	(SELECT AVG(UnitPrice)
 	FROM SalesLT.SalesOrderDetail AS sod
 	WHERE p.ProductID = sod.ProductID)
+ORDER BY P.ProductID;
